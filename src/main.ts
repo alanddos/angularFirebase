cf093f5a5aa724f5f8640c7fe1566b7cc873b6cb
USER <alanddos>
2020-12-22 10:50:28 -0400

    up

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+//import { environment } from './environments/environment';
 
-if (environment.production) {
+//if (environment.production) {
   enableProdMode();
-}
+//}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-  if ('serviceWorker' in navigator && environment.production) {
+  if ('serviceWorker' in navigator ) { //&& environment.production
     navigator.serviceWorker.register('ngsw-worker.js');
   }
 }).catch(err => console.log(err));
