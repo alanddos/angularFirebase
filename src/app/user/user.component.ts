14a02515babe7d1707b107517bcfc0c83395a88f
USER <alanddos>
2020-12-22 10:25:26 -0400

    up

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-import { AuthService } from '../services/core/auth.service'
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  providers: [AuthService]
+  providers: []
 })
 export class UserComponent implements OnInit {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
   login() {
-    let token = this.auth.signInWithPopup(new auth.GoogleAuthProvider());
-    console.log(token)
   }
   logout() {
-    this.auth.signOut();
+
   }
 
 }
